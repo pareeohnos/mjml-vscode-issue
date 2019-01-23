95f2ba65e14a23da6abd16a92dd41b12d059b35f
Adrian Hooper <adrian@euphoricpanda.co.uk>
2019-01-23 11:35:06 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MjmlVscodeIssue::Application.config.secret_token = '16285dd49101e432446c0a31bc13817107e70eb20fb14ed803e48c669ed24ac2f6d22797f582664254528c07449aefce22c0f51c0e17c88733415bf096030e0f'
